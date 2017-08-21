cab1c98a819c3e5710f8e1a995183f28b486aeb7
ItsDeadEnd <adrianppreyra@gmail.com>
2017-08-21 04:24:40 +0000

    fixed a bug where the mod dosent get recognised

--- src/main/java/ElectroDead/total_overhaul/Reference.java
+++ src/main/java/ElectroDead/total_overhaul/Reference.java
@@ -5,8 +5,8 @@ public class Reference {
 	public static final String MOD_ID = "to";
 	public static final String NAME = "Total Overhaul";
 	//Mod version.bugfix version
-	public static final String VERSION = "0.0";
-	public static final String ACCEPTED_VERSION = "[1.12)";
+	public static final String VERSION = "1.0";
+	public static final String ACCEPTED_VERSION = "[1.12]";
 	
 	public static final String CLIENT_PROXY_CLASS = "ElectroDead.total_overhaul.proxy.ClientProxy";
 	public static final String SERVER_PROXY_CLASS = "ElectroDead.total_overhaul.proxy.ServerProxy";
