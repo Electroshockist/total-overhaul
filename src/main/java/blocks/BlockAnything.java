3822a59dc478f250b0bbbbcd9c611aac21c25db4
ItsDeadEnd <adrianppreyra@gmail.com>
2017-09-02 05:15:37 +0000

    added some stuff

---
+++ src/main/java/blocks/BlockAnything.java
@@ -0,0 +1,10 @@
+package blocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class BlockAnything extends Block{
+	public BlockAnything() {
+		super(Material.ROCK);
+	}
+}
