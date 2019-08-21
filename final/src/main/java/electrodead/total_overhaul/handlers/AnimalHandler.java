6a9505ec3decafab796f94c952c8a3ee3a07ab9b
Electroshockist <ericonfire2@yahoo.com>
2019-08-21 19:09:24 -0400

    Added natural passive mob breeding and dying

---
+++ final/src/main/java/electrodead/total_overhaul/handlers/AnimalHandler.java
@@ -0,0 +1,43 @@
+package electrodead.total_overhaul.handlers;
+
+import java.util.Random;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.passive.AbstractHorse;
+import net.minecraft.entity.passive.EntityAnimal;
+import net.minecraft.entity.passive.EntityTameable;
+
+//handles natural breeding and dying of animals
+public class AnimalHandler {
+	private static Random rand = new Random();
+	private static int deathRate = 10000, breedRate = 2500;
+
+	public static void onRecieveAnimal(Entity entity) {
+		//do not run if animal is tameable or a horse
+		if (entity instanceof EntityTameable || entity instanceof AbstractHorse)
+			return;
+		if (entity instanceof EntityAnimal) {
+			EntityAnimal e = (EntityAnimal) entity;
+			//only run if mob can despawn
+			if (!e.isNoDespawnRequired()) {
+				//check if animal is an adult
+				if (e.getGrowingAge() == 0) {
+					//1/breedRate chance of running
+					if (rand.nextInt(breedRate) == breedRate - 1) {
+						//check if animal is not already in love
+						if (!e.isInLove()) {
+							e.setInLove(null);
+							//System.out.println(e.getName() + " is now in love");
+							return;
+						}
+					}
+					//1/deathRate chance of running
+					if (rand.nextInt(deathRate) == deathRate - 1) {
+						e.setDead();
+						return;
+					}
+				}
+			}
+		}
+	}
+}
--- final/src/main/java/electrodead/total_overhaul/handlers/CustomEventHandler.java
+++ final/src/main/java/electrodead/total_overhaul/handlers/CustomEventHandler.java
@@ -1,40 +1,28 @@
 package electrodead.total_overhaul.handlers;
 
 import electrodead.total_overhaul.visual_effects.VoidFog;
-import net.minecraft.client.Minecraft;
 import net.minecraft.entity.item.EntityXPOrb;
 import net.minecraft.entity.passive.EntityBat;
 import net.minecraft.world.World;
+import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
 import net.minecraftforge.event.entity.living.LivingExperienceDropEvent;
-import net.minecraftforge.event.entity.player.EntityItemPickupEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent.ClientTickEvent;
 
 public class CustomEventHandler {
 	World world;
-
-	@SubscribeEvent
-	public void onItemPickup(EntityItemPickupEvent event) {
-//		System.out.println("Item picked up!");
-//		world = event.getEntity().world;
-//		int i = 1 + world.rand.nextInt(3);
-//		while (i > 0) {
-//			int j = EntityXPOrb.getXPSplit(i);
-//			i -= j;
-//			world.spawnEntity(
-//					new EntityXPOrb(world, event.getEntity().posX, event.getEntity().posY, event.getEntity().posZ, j));
-//		}
-	}
+	
+	AnimalHandler animalHandler;
 
 	@SubscribeEvent
 	public void onClientTickEvent(ClientTickEvent event) {
 		VoidFog.update();
 	}
-
-	//
-	// @SubscribeEvent
-	// public void onCheckSpawn(LivingSpawnEvent.CheckSpawn event){
-	// }
+	
+	 @SubscribeEvent
+	 public void onCheckSpawn(LivingUpdateEvent event) {
+		 AnimalHandler.onRecieveAnimal(event.getEntity());
+	 }
 
 	@SubscribeEvent
 	public void onLivingExperienceDrop(LivingExperienceDropEvent event) {
